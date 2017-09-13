c9591fd0fdbfe45bf2cfacceb1f17e7bbd03966e
Harold Ekstrom <hekstrom@savvius.com>
2017-09-13 16:44:15 -0400

    more

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import "./App.css"
+import menu from "./menu.svg"
 import home from "./home.svg"
 import phone from "./phone-sound.svg"
 import cloud from "./cloud.svg"
@@ -8,43 +9,122 @@ import globalnet from "./globe-connections.svg"
 
 class NavItem extends Component {
   render() {
+    const { id, active, icon, title } = this.props
+    const className = active ? "nav-item active" : "nav-item"
+    //console.log("id=", id, "active=", active)
     return (
-      <button className="nav-button">
-        <img src={this.props.icon} alt="" />
-      </button>
+      <div id={id} className={className}>
+        <button onClick={e => this.props.onNavClick(id)}>
+          <img src={icon} height="18" width="18" alt="" />
+          {title && <div>{title}</div>}
+        </button>
+      </div>
     )
   }
 }
 
 class Nav extends Component {
   render() {
+    const { active, expand, onNavClick } = this.props
+    const items = [
+      { id: "menu", icon: menu, title: null },
+      { id: "home", icon: home, title: "Home" },
+      { id: "devices", icon: phone, title: "Devices" },
+      { id: "cloud", icon: cloud, title: "Cloud" },
+      { id: "globalnet", icon: globalnet, title: "Global Network" },
+      { id: "settings", icon: gear, title: null },
+    ]
     return (
-      <nav>
-        <NavItem id={"home"} icon={home} title="Home" />
-        <NavItem id={"phone"} icon={phone} title="Devices" />
-        <NavItem id={"cloud"} icon={cloud} title="Cloud" />
-        <NavItem id={"globalnet"} icon={globalnet} title="Global Network" />
-        <NavItem id={"settings"} icon={gear} title="Settings" />
+      <nav className="nav">
+        {items.map((item, i) => {
+          return (
+            <NavItem
+              key={i}
+              id={item.id}
+              onNavClick={onNavClick}
+              active={item.id === active}
+              icon={item.icon}
+              title={expand ? item.title : null}
+            />
+          )
+        })}
       </nav>
     )
+  }
+}
+
+class HomeTab extends Component {
+  render() {
+    return <div>Home Tab</div>
+  }
+}
+
+class DevicesTab extends Component {
+  render() {
+    return <div>Devices Tab</div>
+  }
+}
+
+class CloudTab extends Component {
+  render() {
+    return <div>Cloud Tab</div>
+  }
+}
+
+class GlobalNetworkTab extends Component {
+  render() {
+    return <div>Global Network Tab</div>
   }
 }
 
 class App extends Component {
   constructor(props) {
     super(props)
+    this.onSwitchTab = this.onSwitchTab.bind(this)
     this.state = {
-      navPage: "home",
+      navTab: "home",
+      navExpand: true,
+    }
+  }
+
+  onSwitchTab(tab) {
+    if (tab === "menu") {
+      this.setState({
+        navExpand: !this.state.navExpand,
+      })
+    } else {
+      this.setState({
+        navTab: tab,
+      })
     }
   }
 
   render() {
+    let tab = null
+    switch (this.state.navTab) {
+      case "home":
+        tab = <HomeTab />
+        break
+      case "devices":
+        tab = <DevicesTab />
+        break
+      case "cloud":
+        tab = <CloudTab />
+        break
+      case "globalnet":
+        tab = <GlobalNetworkTab />
+        break
+      default:
+        break
+    }
     return (
       <div className="app">
-        <header>
-          <h2>Welcome to Foo</h2>
-        </header>
-        <Nav />
+        <Nav
+          active={this.state.navTab}
+          expand={this.state.navExpand}
+          onNavClick={this.onSwitchTab}
+        />
+        <main className="tab">{tab}</main>
       </div>
     )
   }
