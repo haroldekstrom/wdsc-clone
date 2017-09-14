661a9b53c948d33ef734721063b3ee38dad58164
Harold Ekstrom <hekstrom@savvius.com>
2017-09-14 10:15:33 -0400

    tab content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react"
 import "./App.css"
-import menu from "./menu.svg"
-import home from "./home.svg"
-import phone from "./phone-sound.svg"
-import cloud from "./cloud.svg"
-import gear from "./gear.svg"
-import globalnet from "./globe-connections.svg"
+import menuIcon from "./menu.svg"
+import homeIcon from "./home.svg"
+import shieldIcon from "./phone-sound.svg"
+import healthIcon from "./cloud.svg"
+import firewallIcon from "./globe-connections.svg"
+import appIcon from "./globe-connections.svg"
+import familyIcon from "./globe-connections.svg"
+import gearIcon from "./gear.svg"
 
 class NavItem extends Component {
   render() {
@@ -27,12 +29,14 @@ class Nav extends Component {
   render() {
     const { active, expand, onNavClick } = this.props
     const items = [
-      { id: "menu", icon: menu, title: null },
-      { id: "home", icon: home, title: "Home" },
-      { id: "devices", icon: phone, title: "Devices" },
-      { id: "cloud", icon: cloud, title: "Cloud" },
-      { id: "globalnet", icon: globalnet, title: "Global Network" },
-      { id: "settings", icon: gear, title: null },
+      { id: "menu", icon: menuIcon, title: null },
+      { id: "home", icon: homeIcon, title: "Home" },
+      { id: "protection", icon: shieldIcon, title: "Virus & threat protection" },
+      { id: "health", icon: healthIcon, title: "Device performance & health" },
+      { id: "firewall", icon: firewallIcon, title: "Firewall & network protection" },
+      { id: "app", icon: appIcon, title: "App & browser control" },
+      { id: "family", icon: familyIcon, title: "Family options" },
+      { id: "settings", icon: gearIcon, title: null },
     ]
     return (
       <nav className="nav">
@@ -55,25 +59,111 @@ class Nav extends Component {
 
 class HomeTab extends Component {
   render() {
-    return <div>Home Tab</div>
+    return (
+      <div className="hometab">
+        <div>
+          <div>Your device is being protected.</div>
+          <div>
+            Last threat scan: 9/13/2017<br />
+            Last threat definition update: 9/13/2017<br />
+            Last health scan: 9/14/2017
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
-class DevicesTab extends Component {
+class ProtectionTab extends Component {
   render() {
-    return <div>Devices Tab</div>
+    return (
+      <div className="protectiontab">
+        <div className="tab-header">
+          <div className="tab-title">
+            <img src={shieldIcon} height="32" width="32" alt="" />
+            <div>Virus & threat protection</div>
+          </div>
+          <div className="tab-subtitle">
+            View threat history, scan for viruses and other threats, specify protection settings,
+            and get protection udpates.
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
-class CloudTab extends Component {
+class HealthTab extends Component {
   render() {
-    return <div>Cloud Tab</div>
+    return (
+      <div className="healthtab">
+        <div className="tab-header">
+          <div className="tab-title">
+            <img src={healthIcon} height="32" width="32" alt="" />
+            <div>Device performance & health</div>
+          </div>
+          <div className="tab-subtitle">
+            Check that your Windows is up-to-date and if there are any issues impacting your device
+            health. The Health report shows the status of the most recent scan.
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
-class GlobalNetworkTab extends Component {
+class FirewallTab extends Component {
   render() {
-    return <div>Global Network Tab</div>
+    return (
+      <div className="firewalltab">
+        <div className="tab-header">
+          <div className="tab-title">
+            <img src={firewallIcon} height="32" width="32" alt="" />
+            <div>Firewall & network protection</div>
+          </div>
+          <div className="tab-subtitle">
+            View network connections, specify Windows Firewall settings, and troubleshoot network
+            and Internet problems.
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class AppTab extends Component {
+  render() {
+    return (
+      <div className="apptab">
+        <div className="tab-header">
+          <div className="tab-title">
+            <img src={appIcon} height="32" width="32" alt="" />
+            <div>App & browser control</div>
+          </div>
+          <div className="tab-subtitle">
+            Set up Windows Defender SmartScreen settings for apps and browsers.
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class FamilyTab extends Component {
+  render() {
+    return (
+      <div className="familytab">
+        <div className="tab-header">
+          <div className="tab-title">
+            <img src={familyIcon} height="32" width="32" alt="" />
+            <div>Family options</div>
+          </div>
+          <div className="tab-subtitle">
+            Get what you need to simplify your family's digital life.
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
@@ -82,7 +172,7 @@ class App extends Component {
     super(props)
     this.onSwitchTab = this.onSwitchTab.bind(this)
     this.state = {
-      navTab: "home",
+      navTab: "protection",
       navExpand: true,
     }
   }
@@ -105,14 +195,20 @@ class App extends Component {
       case "home":
         tab = <HomeTab />
         break
-      case "devices":
-        tab = <DevicesTab />
+      case "protection":
+        tab = <ProtectionTab />
         break
-      case "cloud":
-        tab = <CloudTab />
+      case "health":
+        tab = <HealthTab />
         break
-      case "globalnet":
-        tab = <GlobalNetworkTab />
+      case "firewall":
+        tab = <FirewallTab />
+        break
+      case "app":
+        tab = <AppTab />
+        break
+      case "family":
+        tab = <FamilyTab />
         break
       default:
         break
